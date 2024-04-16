c6b366c449aab96e412f3b06d194921cd5ca1ec6
Adam Stankiewicz <agstanki@gmail.com>
2024-04-16 10:50:35 -0400

    feat: define custom logging service in env.config.js

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,12 +1,20 @@
 import { Container } from '@openedx/paragon';
+import { logError } from '@edx/frontend-platform/logging';
+import { useEffect } from 'react';
 
-const ExamplePage = () => (
-  <main>
-    <Container className="py-5">
-      <h1>Example Page</h1>
-      <p>Hello world!</p>
-    </Container>
-  </main>
-);
+const ExamplePage = () => {
+  useEffect(() => {
+    // [temp] Test custom logError
+    logError(new Error('oh noes!!!'));
+  }, []);
+  return (
+    <main>
+      <Container className="py-5">
+        <h1>Example Page</h1>
+        <p>Hello world!</p>
+      </Container>
+    </main>
+  );
+};
 
 export default ExamplePage;
